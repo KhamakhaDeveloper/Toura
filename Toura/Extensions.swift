3109a6b8741b3012303d0ad0b2664d0281fc0854
Carpenter, Deepak (US - Bengaluru) <decarpenter@deloitte.com>
2017-12-02 18:51:15 +0530

    Committed Changes for image Tile

--- Toura/Extensions.swift
+++ Toura/Extensions.swift
@@ -133,3 +133,26 @@ extension UIViewController{
         self.navigationController?.navigationBar.backgroundColor = UIColor.clear
     }
 }
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) -> UIImage{
+        var dImage = UIImage()
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+                dImage = image
+            }
+            }.resume()
+        return dImage
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
